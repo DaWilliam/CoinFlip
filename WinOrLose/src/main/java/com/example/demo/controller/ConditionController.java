a61b802fb997d45229b7bdad2b3b388ae22cb140
Jordan <jordanclarkemba@gmail.com>
2020-02-04 14:43:04 -0500

    Zuul

--- WinOrLose/src/main/java/com/example/demo/controller/ConditionController.java
+++ WinOrLose/src/main/java/com/example/demo/controller/ConditionController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/conditions")
 public class ConditionController {
 	
 	@Autowired
