34180db48604193f45342a14556e1d7a99f78ab9
Jordan <jordanclarkemba@gmail.com>
2020-02-05 15:56:41 -0500

    cross origins

--- Flips/src/main/java/com/example/demo/controller/Checks2Controller.java
+++ Flips/src/main/java/com/example/demo/controller/Checks2Controller.java
@@ -2,10 +2,12 @@ package com.example.demo.controller;
 
 import java.util.Random;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/flips")
 public class Checks2Controller {
--- WinOrLose/src/main/java/com/example/demo/controller/ConditionController.java
+++ WinOrLose/src/main/java/com/example/demo/controller/ConditionController.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/conditions")
 public class ConditionController {
